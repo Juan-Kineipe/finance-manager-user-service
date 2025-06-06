95bf46223352d2b7355b9fd9ba951a2213f65582
Juan Kineipe <juan.kineipe@gmail.com>
2025-06-06 09:12:09 -0300

    initial commit

---
+++ src/main/java/com/kineipe/financemanageruserservice/FinanceManagerUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kineipe.financemanageruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagerUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagerUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kineipe/financemanageruserservice/FinanceManagerUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kineipe.financemanageruserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
